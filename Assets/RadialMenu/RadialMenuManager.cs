4e813ac40146e708fda716a063ebf9313e08bf87
MrYapikZ <yafialhaidar18@gmail.com>
2024-11-15 14:31:50 +0700

    Radial system done

--- Assets/RadialMenu/RadialMenuManager.cs
+++ Assets/RadialMenu/RadialMenuManager.cs
@@ -1,5 +1,7 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 namespace FloxyDev.RadialMenu
@@ -10,26 +12,79 @@ namespace FloxyDev.RadialMenu
         [SerializeField, Range(0, 100)] private float radialPartsGap;
         [SerializeField] private GameObject radialPartPrefab;
         [SerializeField] private Transform radialPartsCanvas;
+        [SerializeField] private Transform handTransform;
         
-        private readonly List<GameObject> _spawnedRadialParts = new List<GameObject>();
+        public UnityEvent<int> onPartSelected;
 
+        private readonly List<GameObject> _spawnedRadialParts = new List<GameObject>();
+        private int _currentSelectedRadialPart;
+        
         private void Update()
         {
-            SpawnRadialPart();
+            if (Input.GetMouseButtonDown(0))
+            {
+                SpawnRadialPart();
+            }
+
+            if (Input.GetMouseButton(0))
+            {
+                GetSelectedRadialPart();
+            }
+
+            if (Input.GetMouseButtonUp(0))
+            {
+                HideRadialParts();
+            }
         }
-        
-        private void SpawnRadialPart()
+
+        public void HideRadialParts()
         {
+            onPartSelected.Invoke(_currentSelectedRadialPart);
+            radialPartsCanvas.gameObject.SetActive(false);
+        }
+
+        public void GetSelectedRadialPart()
+        {
+            Vector3 centerToHand = handTransform.position - radialPartsCanvas.position;
+            Vector3 centerToHandProjected = Vector3.ProjectOnPlane(centerToHand, radialPartsCanvas.forward);
+
+            float angle = Vector3.SignedAngle(radialPartsCanvas.up, centerToHandProjected, -radialPartsCanvas.forward);
+
+            if (angle < 0) angle += 360;
+
+            _currentSelectedRadialPart = (int)angle * numberOfRadialPart / 360;
+
+            for (int i = 0; i < _spawnedRadialParts.Count; i++)
+            {
+                if (i == _currentSelectedRadialPart)
+                {
+                    _spawnedRadialParts[i].GetComponent<Image>().color = Color.green;
+                    _spawnedRadialParts[i].transform.localScale = 1.1f * Vector3.one;
+                }
+                else
+                {
+                    _spawnedRadialParts[i].GetComponent<Image>().color = Color.white;
+                    _spawnedRadialParts[i].transform.localScale = Vector3.one;
+                }
+            }
+        }
+
+        public void SpawnRadialPart()
+        {
+            radialPartsCanvas.gameObject.SetActive(true);
+            radialPartsCanvas.position = handTransform.position;
+            radialPartsCanvas.rotation = handTransform.rotation;
+            
             foreach (var radialPart in _spawnedRadialParts)
             {
                 Destroy(radialPart);
             }
-            
+
             _spawnedRadialParts.Clear();
-            
+
             for (int i = 0; i < numberOfRadialPart; i++)
             {
-                float angle = i * 360 / (float)numberOfRadialPart - radialPartsGap / 2;
+                float angle = -i * 360 / (float)numberOfRadialPart - radialPartsGap / 2;
                 Vector3 radialPartEulerAngles = new Vector3(0, 0, angle);
 
                 GameObject spawnedRadialPart = Instantiate(radialPartPrefab, radialPartsCanvas);
@@ -38,7 +93,7 @@ namespace FloxyDev.RadialMenu
 
                 spawnedRadialPart.GetComponent<Image>().fillAmount =
                     (1 / (float)numberOfRadialPart) - (radialPartsGap / 360);
-                
+
                 _spawnedRadialParts.Add(spawnedRadialPart);
             }
         }
